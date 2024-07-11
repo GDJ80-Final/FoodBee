0154eb82da877e9e410b7de6e91ef1700201d93f
DESKTOP-G20BGD1\서기범 <rlqja1221@naver.com>
2024-07-11 12:03:13 +0900

    [서기범] Feat: TeamColor Class 추가

---
+++ FoodBee/src/main/java/com/gd/foodbee/util/TeamColor.java
@@ -0,0 +1,10 @@
+package com.gd.foodbee.util;
+
+public class TeamColor {
+	public static String RED = "\u001B[41m";
+	public static String BLUE = "\u001B[44m";
+	public static String GREEN = "\u001B[32m";
+	public static String YELLOW = "\u001B[33m";
+	public static String PURPLE = "\u001B[35m";
+	public static String RESET = "\u001B[0m";
+}
